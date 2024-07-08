6eb11ae9faee3a0e8920cadd21266a988b9227f5
Artem <zinykov_a@mail.ru>
2024-07-08 21:25:46 +0300

    Добавил обработчик исключений при скачивании книг

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import requests
 from pathlib import Path
 
 
-books_id = [32168, 32169, 32170, 123, 222, 32131, 4444, 51255, 55555, 72714]
+books_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError("Redirect")
+    return response
+
 
 Path("./books").mkdir(parents=True, exist_ok=True)
 for book_id in books_id:
     url = f"https://tululu.org/txt.php?id={book_id}"
-    response = requests.get(url)
-    response.raise_for_status()
+    try:
+        response = requests.get(url)
+        response = check_for_redirect(response)
+        filename = f'./books/book-{book_id}.txt'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+    except requests.RequestException:
+        print(f"Ошибка при запросе книги {book_id}")
 
-    filename = f'./books/book-{book_id}.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
