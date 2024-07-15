bb663a6ae607d2c1b1572f578550e4482cf1e2d5
Artem <zinykov_a@mail.ru>
2024-07-15 21:29:55 +0300

    Download books from all pages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import time
+import json
 
 import requests
 from pathlib import Path
@@ -20,6 +21,13 @@ def fetch_book_page(url, book_id):
     url_book = f"{url}/b{book_id}/"
     response = requests.get(url_book)
     check_for_redirect(response)
+    response.raise_for_status()
+    return BeautifulSoup(response.text, 'lxml')
+
+
+def fetch_books_by_genre(page):
+    url_book = f"https://tululu.org/l55/{page}/"
+    response = requests.get(url_book)
     response.raise_for_status()
     return BeautifulSoup(response.text, 'lxml')
 
@@ -78,6 +86,33 @@ def download_image(filename, image_url, folder='images/'):
         file.write(response.content)
 
 
+def download_book_from_page(soup):
+    content_div = soup.find_all('div', class_="bookimage")
+    book_ids = []
+    for book_div in content_div:
+        content = book_div.find("a")
+
+        relative_url = content["href"]
+        relative_url_parts = urlparse(relative_url)
+        book_ids.append(relative_url_parts.path.split("/b")[1])
+    return book_ids
+
+
+def download_all_book(book_ids):
+    for book_id in book_ids:
+        try:
+            soup = fetch_book_page(URL, book_id)
+            title, author = get_author_and_title(soup)
+            download_txt(URL, book_id, title, folder='books/')
+            image_url = get_image(soup, book_id)
+            download_image(title, image_url)
+            book_comments = get_book_comments(soup)
+            book_genres = get_book_genres(soup)
+            console_output(title, author, book_comments, book_genres)
+        except (AttributeError, requests.RequestException) as err:
+            print(f"Ошибка загрузки книги - {book_id}: {err}")
+
+
 def console_output(title, author, book_comments, book_genres):
     print(f"Название: {title}\nАвтор: {author}")
     print("\nЖанр книги: ")
@@ -91,9 +126,13 @@ def console_output(title, author, book_comments, book_genres):
 def main():
     Path("./books").mkdir(parents=True, exist_ok=True)
     parser = argparse.ArgumentParser(prog='main', description='запускает скрипт для скачивания книг')
-    parser.add_argument('start_id', default=1, help="Укажите начальный ID книги для скачивания",
+    parser.add_argument('--start_id', default=1, help="Укажите начальный ID книги для скачивания",
                         type=int)
-    parser.add_argument('end_id', default=11, help="Укажите конечный ID книги для скачивания",
+    parser.add_argument('--end_id', default=11, help="Укажите конечный ID книги для скачивания",
+                        type=int)
+    parser.add_argument('--start_page', help="Укажите начальную страницу с книгами для скачивания",
+                        type=int)
+    parser.add_argument('--end_page', help="Укажите конечную страницу с книгами для скачивания",
                         type=int)
     parser_args = parser.parse_args()
     for book_id in range(parser_args.start_id, parser_args.end_id+1):
@@ -115,6 +154,19 @@ def main():
                 attempt += 1
             except (AttributeError, requests.RequestException) as err:
                 print(f"Ошибка загрузки книги - {book_id}: {err}")
+                break
+    for book_page in range(parser_args.start_page, parser_args.end_page):
+        attempt = 0
+        while True:
+            try:
+                soup = fetch_books_by_genre(book_page)
+                book_ids = download_book_from_page(soup)
+                download_all_book(book_ids)
+                break
+            except requests.ConnectionError as err:
+                print(f"Ошибка соединения для книги - {book_page} (попытка {attempt + 1}): {err}")
+                time.sleep(10)
+                attempt += 1
 
 
 if __name__ == "__main__":
